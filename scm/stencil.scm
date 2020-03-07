hanwen/lilypond

commit c1a4e443791bf7fc7228dc2e9ee77ead84654a71

don't do expression comparison.

scm/stencil.scm
@@ -332,7 +332,6 @@ grestore
 	  (write-system-signature outname (car paper-systems))
 	  (write-system-signatures basename (cdr paper-systems) (1+ count))))))
 
-
 (use-modules (scm paper-system))
 (define-public (write-system-signature filename paper-system)
   (define (float? x)
@@ -342,7 +341,9 @@ grestore
     (paper-system-system-grob paper-system))
   
   (define output (open-output-file filename))
-  
+
+  ;; todo: optionally use a command line flag? Or just junk this?
+  (define compare-expressions #f)
   (define (strip-floats expr)
     "Replace floats by #f"
     (cond
@@ -364,16 +365,6 @@ grestore
 	      rest))
 	expr))
   
-
-  (define (pythonic-string expr)
-    "escape quotes and slashes for python consumption"
-    (regexp-substitute/global #f "([\n\\\\'\"])" (format "~a" expr) 'pre "\\" 1 'post))
-
-  (define (pythonic-pair expr)
-    (format "(~a,~a)"
-	    (car expr) (cdr expr)))
-
-
   (define (raw-string expr)
     "escape quotes and slashes for python consumption"
     (regexp-substitute/global #f "[@\n]" (format "~a" expr) 'pre " " 'post))
@@ -394,11 +385,13 @@ grestore
 	 ;; todo: use stencil extent if available.
 	 (x-ext (ly:grob-extent grob system-grob X))
 	 (y-ext (ly:grob-extent grob system-grob Y))
-	 )
-
-      (interpret-for-signature #f (lambda (e)
-				    (set! collected (cons e collected)))
-			       rest)
+	 (expression-skeleton
+	  (if compare-expressions
+	      (interpret-for-signature
+	       #f (lambda (e)
+		    (set! collected (cons e collected)))
+	       rest)
+	     "")))
 
       (format output
 	      "~a@~a@~a@~a@~a\n"

commit 973677710c4f7b4551a0283f1084dfa68c4357d0

output-distance: remove support for stencil expression checks
This must have been broken since commit c1a4e44 "don't do expression
comparison." (Jun 2007).

This also caused a self-test failure in output-distance, which means
it hasn't been run for 13 years.

Adapt for expression/origin removal.  Make test_compare_signatures pass,

scm/stencil.scm
@@ -1047,40 +1047,10 @@ grestore
 
 (use-modules (scm paper-system))
 (define-public (write-system-signature filename paper-system)
-  (define (float? x)
-    (and (number? x) (inexact? x)))
-
   (define system-grob
     (paper-system-system-grob paper-system))
 
   (define output (open-output-file filename))
-
-  ;; todo: optionally use a command line flag? Or just junk this?
-  (define compare-expressions #f)
-  (define (strip-floats expr)
-    "Replace floats by #f"
-    (cond
-     ((float? expr) #f)
-     ((ly:font-metric? expr) (ly:font-name expr))
-     ((pair? expr) (cons (strip-floats (car expr))
-                         (strip-floats (cdr expr))))
-     (else expr)))
-
-  (define (fold-false-pairs expr)
-    "Try to remove lists of #f as much as possible."
-    (if (pair? expr)
-        (let*
-            ((first (car expr))
-             (rest (fold-false-pairs (cdr expr))))
-
-          (if first
-              (cons (fold-false-pairs first) rest)
-              rest))
-        expr))
-
-  (define (raw-string expr)
-    "escape quotes and slashes for python consumption"
-    (regexp-substitute/global #f "[@\n]" (simple-format #f "~a" expr) 'pre " " 'post))
 
   (define (raw-pair expr)
     (simple-format #f "~a ~a"
@@ -1089,33 +1059,18 @@ grestore
   (define (found-grob expr)
     (let*
         ((grob (car expr))
-         (rest (cdr expr))
-         (collected '())
-         (cause (event-cause grob))
-         (input (if (ly:stream-event? cause) (ly:event-property cause 'origin) #f))
-         (location (if (ly:input-location? input) (ly:input-file-line-char-column input) '()))
-
-         ;; todo: use stencil extent if available.
          (x-ext (ly:grob-extent grob system-grob X))
-         (y-ext (ly:grob-extent grob system-grob Y))
-         (expression-skeleton
-          (if compare-expressions
-              (interpret-for-signature
-               #f (lambda (e)
-                    (set! collected (cons e collected)))
-               rest)
-              "")))
+         (y-ext (ly:grob-extent grob system-grob Y)))
 
       (simple-format output
-                     "~a@~a@~a@~a@~a\n"
+                     "~a@~a@~a\n"
                      (cdr (assq 'name (ly:grob-property grob 'meta) ))
-                     (raw-string location)
                      (raw-pair (if (interval-empty? x-ext) '(1 . -1) x-ext))
                      (raw-pair (if (interval-empty? y-ext) '(1 . -1) y-ext))
-                     (raw-string collected))
+                     )
       ))
 
-  (define (interpret-for-signature escape collect expr)
+  (define (interpret-for-signature handle-grob-cause expr)
     (define (interpret expr)
       (let*
           ((head (if (pair? expr)
@@ -1123,7 +1078,7 @@ grestore
                      #f)))
 
         (cond
-         ((eq? head 'grob-cause) (escape (cdr expr)))
+         ((eq? head 'grob-cause) (handle-grob-cause (cdr expr)))
          ((eq? head 'color) (interpret (caddr expr)))
          ((eq? head 'rotate-stencil) (interpret (caddr expr)))
          ((eq? head 'translate-stencil) (interpret (caddr expr)))
@@ -1131,11 +1086,7 @@ grestore
          ;; the expression interpreted.  Right?
          ((eq? head 'with-outline) (interpret (cadr expr)))
          ((eq? head 'combine-stencil)
-          (for-each interpret  (cdr expr)))
-         (else
-          (collect (fold-false-pairs (strip-floats expr))))
-
-         )))
+          (for-each interpret  (cdr expr))))))
 
     (interpret expr))
 
@@ -1143,9 +1094,9 @@ grestore
       (begin
         (display (simple-format #f "# Output signature\n# Generated by LilyPond ~a\n" (lilypond-version))
                  output)
-        (interpret-for-signature found-grob (lambda (x) #f)
+        (interpret-for-signature found-grob
                                  (ly:stencil-expr
                                   (paper-system-stencil paper-system)))))
 
-  ;; should be superfluous, but leaking "too many open files"?
+  ;; Close explicitly so we don't run out of file descriptors
   (close-port output))

scripts/build/output-distance.py
@@ -23,7 +23,6 @@ X_AXIS = 0
 Y_AXIS = 1
 INFTY = 1e6
 
-OUTPUT_EXPRESSION_PENALTY = 1
 ORPHAN_GROB_PENALTY = 1
 options = None
 
@@ -155,10 +154,9 @@ def difference_area (a, b):
     return bbox_area (a) - bbox_area (bbox_intersection (a,b))
 
 class GrobSignature:
-    """A (grob-name, output-expression, bbox) tuple"""
+    """A (grob-name, bbox) tuple"""
     def __init__ (self, exp_list):
-        (self.name, _, bbox_x,
-         bbox_y, self.output_expression) = tuple (exp_list)
+        (self.name, bbox_x, bbox_y) = tuple (exp_list)
 
         self.bbox = (bbox_x, bbox_y)
         self.centroid = ((bbox_x[0] + bbox_x[1])/2.0,
@@ -182,12 +180,6 @@ class GrobSignature:
                     difference_area (other.bbox, self.bbox)) / divisor
         else:
             return 0.0
-
-    def expression_distance (self, other):
-        if self.output_expression == other.output_expression:
-            return 0
-        else:
-            return 1
 
 
 class SystemSignature:
@@ -295,21 +287,6 @@ class SystemLink:
 
         self._orphan_count = count
 
-    def calc_output_exp_distance (self):
-        if self.system1 and self.system2:
-            d = 0
-        else:
-            d = 100 * (self.system1 != self.system2)
-
-        for (g1, g2) in list(self.back_link_dict.items ()):
-            if g2:
-                d += g1.expression_distance (g2)
-
-        self._expression_change_count = d
-
-    def output_expression_details_string (self):
-        return ', '.join ([g1.name for g1 in self.expression_changed])
-
     def geo_details_string (self):
         results = [(d, g1,g2) for ((g1, g2), d) in list(self.geo_distances.items())]
         # Only compare distances.
@@ -331,14 +308,8 @@ class SystemLink:
 
         return self._orphan_count
 
-    def output_expression_change_count (self):
-        if self._expression_change_count == None:
-            self.calc_output_exp_distance ()
-        return self._expression_change_count
-
     def distance_tuple (self):
-        return (self.output_expression_change_count (),
-                self.orphan_count (),
+        return (self.orphan_count (),
                 self.geometric_distance ())
 
 
@@ -366,8 +337,8 @@ def read_signature_file (name):
 
     def string_to_entry (s):
         fields = s.split('@')
+        fields[1] = string_to_tup (fields[1])
         fields[2] = string_to_tup (fields[2])
-        fields[3] = string_to_tup (fields[3])
 
         return tuple (fields)
 
@@ -852,8 +823,7 @@ class SignatureFileLink (FileLink):
             html += e
 
             e = '<td>%d</td>' % c
-            for s in (link.output_expression_details_string (),
-                      link.orphan_details_string (),
+            for s in (link.orphan_details_string (),
                       link.geo_details_string ()):
                 e += "<td>%s</td>" % s
 
@@ -873,7 +843,6 @@ class SignatureFileLink (FileLink):
 <table>
 <tr>
 <th>system</th>
-<th>output</th>
 <th>orphan</th>
 <th>geo</th>
 </tr>
@@ -1343,7 +1312,7 @@ def open_write_file (x):
 def system (x):
     log_verbose ('invoking %s' % x)
     stat = os.system (x)
-    assert stat == 0
+    assert stat == 0, (stat, x)
 
 def system_allow_exit1 (x):
     log_verbose ('invoking %s' % x)
@@ -1359,27 +1328,25 @@ def test_paired_files ():
 def test_compare_tree_pairs ():
     system ('rm -rf dir1 dir2')
     system ('mkdir dir1 dir2')
-    system ('cp 20{-*.signature,.ly,.png,.eps,.log,.profile} dir1')
-    system ('cp 20{-*.signature,.ly,.png,.eps,.log,.profile} dir2')
-    system ('cp 20expr{-*.signature,.ly,.png,.eps,.log,.profile} dir1')
-    system ('cp 19{-*.signature,.ly,.png,.eps,.log,.profile} dir2/')
-    system ('cp 19{-*.signature,.ly,.png,.eps,.log,.profile} dir1/')
+    system ('cp 20{-*.signature,.ly,.png,-1.eps,.log,.profile} dir1')
+    system ('cp 20{-*.signature,.ly,.png,-1.eps,.log,.profile} dir2')
+    system ('cp 19{-*.signature,.ly,.png,-1.eps,.log,.profile} dir2/')
+    system ('cp 19{-*.signature,.ly,.png,-1.eps,.log,.profile} dir1/')
     system ('cp 19-1.signature 19.sub-1.signature')
     system ('cp 19.ly 19.sub.ly')
     system ('cp 19.profile 19.sub.profile')
     system ('cp 19.log 19.sub.log')
     system ('cp 19.png 19.sub.png')
-    system ('cp 19.eps 19.sub.eps')
+    system ('cp 19-1.eps 19.sub-1.eps')
 
     system ('cp 20multipage* dir1')
     system ('cp 20multipage* dir2')
-    system ('cp 19multipage-1.signature dir2/20multipage-1.signature')
 
     system ('mkdir -p dir1/subdir/ dir2/subdir/')
-    system ('cp 19.sub{-*.signature,.ly,.png,.eps,.log,.profile} dir1/subdir/')
-    system ('cp 19.sub{-*.signature,.ly,.png,.eps,.log,.profile} dir2/subdir/')
-    system ('cp 20grob{-*.signature,.ly,.png,.eps,.log,.profile} dir2/')
-    system ('cp 20grob{-*.signature,.ly,.png,.eps,.log,.profile} dir1/')
+    system ('cp 19.sub{-*.signature,.ly,.png,-1.eps,.log,.profile} dir1/subdir/')
+    system ('cp 19.sub{-*.signature,.ly,.png,-1.eps,.log,.profile} dir2/subdir/')
+    system ('cp 20grob{-*.signature,.ly,.png,-1.eps,.log,.profile} dir2/')
+    system ('cp 20grob{-*.signature,.ly,.png,-1.eps,.log,.profile} dir1/')
     system ('echo HEAD is 1 > dir1/tree.gittxt')
     system ('echo HEAD is 2 > dir2/tree.gittxt')
 
@@ -1439,10 +1406,6 @@ def test_basic_compare ():
                'extragrob': '',
                'userstring': 'test' },
              { 'papermod' : '',
-               'name' : '20expr',
-               'extragrob': '',
-               'userstring': 'blabla' },
-             { 'papermod' : '',
                'name' : '20grob',
                'extragrob': 'r2. \\break c1',
                'userstring': 'test' },
@@ -1451,25 +1414,26 @@ def test_basic_compare ():
     for d in dicts:
         open (d['name'] + '.ly','w').write (ly_template % d)
 
-    names = [d['name'] for d in dicts]
-
-    system ('lilypond -ddump-profile -dseparate-log-files -ddump-signatures --png -dbackend=eps ' + ' '.join (names))
-
+    simple_names = [d['name'] for d in dicts]
 
     multipage_str = r'''
     #(set-default-paper-size "a6")
+    \book {
     \score {
       \relative c' { c1 \pageBreak c1 }
       \layout {}
       \midi {}
     }
+    \paper {}
+    }
     '''
 
     open ('20multipage.ly', 'w').write (multipage_str.replace ('c1', 'd1'))
     open ('19multipage.ly', 'w').write ('#(set-global-staff-size 19.5)\n' + multipage_str)
-    system ('lilypond -dseparate-log-files -ddump-signatures --png 19multipage 20multipage ')
 
-    test_compare_signatures (names)
+    names = simple_names + [ "20multipage", "19multipage" ]
+    system ('lilypond -ddump-profile -dseparate-log-files -ddump-signatures --png ' + ' '.join (names))
+    test_compare_signatures (simple_names)
 
 
 def test_compare_signatures (names, timing=False):
@@ -1491,9 +1455,13 @@ def test_compare_signatures (names, timing=False):
     t0 = time.time ()
     count = 0
     combinations = {}
+    links = {}
     for (n1, s1) in list(sigs.items()):
         for (n2, s2) in list(sigs.items()):
-            combinations['%s-%s' % (n1, n2)] = SystemLink (s1,s2).distance_tuple ()
+            key ='%s-%s' % (n1, n2)
+            link = SystemLink (s1,s2)
+            links[key] = link
+            combinations[key] = link.distance_tuple ()
             count += 1
 
     if timing:
@@ -1506,10 +1474,11 @@ def test_compare_signatures (names, timing=False):
         for k,v in results:
             print('%-20s' % k, v)
 
-    assert combinations['20-20'] == (0.0,0.0,0.0)
-    assert combinations['20-20expr'][0] > 0.0
-    assert combinations['20-19'][2] < 10.0
-    assert combinations['20-19'][2] > 0.0
+    assert links["20grob-20"].orphan_count() > 0
+    assert combinations['20-20'] == (0.0, 0.0)
+    assert combinations['20-19'][1] < 10.0
+    assert combinations['20-19'][1] > 0.0
+    assert combinations['20grob-20'][0] > 0
 
 
 def run_tests ():